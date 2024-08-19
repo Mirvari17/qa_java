f3e5e505382440657b5df29f2e5877120e1862f4
Mirvari17 <mirka.agaeva@yandex.ru>
2024-08-19 20:16:49 +0300

    Исправлены замечания

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -2,19 +2,28 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.verify;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
+
 public class CatTest {
 
-    public CatTest() {}
-    Feline feline = new Feline();
-    Cat cat = new Cat(feline);
+    // public CatTest() {}
+    @Mock
+    Feline felineMock;
+
+    @InjectMocks
+    Cat cat;
+
+    //Feline feline = new Feline();
+    //Cat cat = new Cat(felineMock);
 
     // кошка издаёт звук "мяу"
     @Test
@@ -25,26 +34,26 @@ public class CatTest {
     // кошка ест еду хищника - "Животные", "Птицы", "Рыба"
     @Test
     public void getFoodTest() throws Exception {
+        when(felineMock.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         assertEquals(cat.getFood(), List.of("Животные", "Птицы", "Рыба"));
     }
 
 
     // мокируем feline и cat
-    @Mock
-    Feline felineMock;
 
-    @Mock
-    Cat catMock = new Cat(felineMock);
 
-    @Test
-    public void getSoundMockedTest() {
-        catMock.getSound();
-        verify(catMock).getSound();
-    }
-
-    @Test
-    public void getFoodMockTest() throws Exception {
-        catMock.getFood();
-        verify(catMock).getFood();
-    }
+//    @Mock
+//    Cat catMock = new Cat(felineMock);
+//
+//    @Test
+//    public void getSoundMockedTest() {
+//        cat.getSound();
+//        verify(cat).getSound();
+//    }
+//
+//    @Test
+//    public void getFoodMockTest() throws Exception {
+//        catMock.getFood();
+//        verify(catMock).getFood();
+//    }
 }
--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -4,52 +4,62 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
 import java.util.List;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LionTest {
 
-    public LionTest() throws Exception {
-    }
-    Lion lion = new Lion("Самка", new Feline());
+    private Lion lion;
+
+    @Mock
+    Feline felineMock;
 
     // у льва 1 котёнок
     @Test
-    public void lionGetKittensTest() {
+    public void lionGetKittensTest() throws Exception {
+        lion = new Lion("Самка", felineMock);
+        when(felineMock.getKittens()).thenReturn(1);
         assertEquals(lion.getKittens(), 1);
     }
 
     // еда льва - "Животные", "Птицы", "Рыба"
     @Test
     public void lionGetFoodTest() throws Exception {
+        lion = new Lion("Самка", felineMock);
+        when(felineMock.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         assertEquals(lion.getFood(), List.of("Животные", "Птицы", "Рыба"));
     }
 
     // лев выбрасывает ошибку, если ему подсунуть не "Самец" или "Самка"
-    @Test (expected = Exception.class)
-    public void lionThrowsExceptionTest() throws Exception {
-        Lion l = new Lion("Абырвалг", new Feline());
+    @Test
+    public void lionThrowsExceptionTest() {
+        try {
+            new Lion("Абырвалг", felineMock);
+        } catch (Exception e) {
+            assertEquals("Используйте допустимые значения пола животного - самец или самка", e.getMessage());
+        }
     }
 
 
     // льва внутри тестов можно создавать с любыми параметрами
     // feline мокируем
-    @Mock
-    Feline felineMock;
 
-    @Test
-    public void lionGetKittensMockedTest() throws Exception {
-        Lion l = new Lion("Самка", felineMock);
-        l.getKittens();
-        verify(felineMock).getKittens();
-    }
 
-    @Test
-    public void lionGetFoodMockedTest() throws Exception {
-        Lion l = new Lion("Самец", felineMock);
-        l.getFood();
-        verify(felineMock).getFood("Хищник");
-    }
+//    @Test
+//    public void lionGetKittensMockedTest() throws Exception {
+//        Lion l = new Lion("Самка", felineMock);
+//        l.getKittens();
+//        verify(felineMock).getKittens();
+//    }
+//
+//    @Test
+//    public void lionGetFoodMockedTest() throws Exception {
+//        Lion l = new Lion("Самец", felineMock);
+//        l.getFood();
+//        verify(felineMock).getFood("Хищник");
+//    }
 }
